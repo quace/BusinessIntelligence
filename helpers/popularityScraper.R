28fffd62d136779bd5ef6506476628a8a03d772c
Leen Jooken <leen.jooken@student.uhasselt.be>
2017-11-03 22:32:43 +0100

    Start of popularity scraper
    name and src

---
+++ helpers/popularityScraper.R
@@ -0,0 +1,18 @@
+library("rvest")
+library(XML)
+
+#Scrape the most popular players
+url2 <- "https://www.futbin.com/popular"
+popular <- url2 %>%
+  html() %>%
+  html_nodes(xpath='
+             /html/body/div[7]/div[4]/ul') 
+#%>%
+# html_text()
+
+
+hh <- htmlParse(popular,asText=T)
+
+#use xpath to extract data
+names <- xpathSApply(hh,'//*[@id="Player-card"]/div[3]',xmlValue)
+
