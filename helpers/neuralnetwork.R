dbdce33b8b10888e48145ec203a45d6ecb3839f9
quace <quinten.moesen@gmail.com>
2017-11-28 18:58:37 +0100

    neural network opgeruimd met linear model voor prof. Napoles

--- helpers/neuralnetwork.R
+++ helpers/neuralnetwork.R
@@ -1,6 +1,10 @@
 
 ##### ETL
-regressionData <- fullData[c("Contract_Expiry", "Rating", "Height", "Weight", "Age", "ValueUnified", "WageUnified")]
+
+fullData <- read.csv("MergedCompleteDataNEW.csv")
+
+keep <- c("Contract_Expiry", "Rating", "Height", "Weight", "Age", "Potential", "Pace", "Shooting", "Passing", "Dribblingx","Defending","Physicality","GK_Score","Preffered_Position","ValueUnified", "WageUnified")
+regressionData <- fullData[keep]
 regressionData$Height <- gsub("[^0-9]", "", regressionData$Height)
 regressionData$Weight <- gsub("[^0-9]", "", regressionData$Weight)
 
@@ -8,35 +12,86 @@ regressionData$Height <- as.numeric(as.character(regressionData$Height))
 regressionData$Weight <- as.numeric(as.character(regressionData$Weight))
 
 regressionData$ValueUnified <- as.numeric(regressionData$ValueUnified)
+regressionData$Preffered_Position <- gsub("/"," ", regressionData$Preffered_Position)
+regressionData$Position <- word(regressionData$Preffered_Position, 1)
 
-##### Regression
-set.seed(1000)
+GK <- c("GK")
+DEF <- c("LB","CB","RB", "RWB", "LWB")
+MF <- c("CDM", "CM", "CAM", "RM", "LM")
+ATT <- c("RW", "LW", "CF", "ST")
+
+keep2 <- c("Contract_Expiry", "Rating", "Height", "Weight", "Age", "Potential", "Pace", "Shooting", "Passing", "Dribblingx","Defending","Physicality","GK_Score","Position")
+regData <- regressionData[keep2]
+regData <- regData %>% mutate(GK_Bool = ifelse(regData$Position %in% GK,1,0))
+regData <- regData %>% mutate(DEF_Bool = ifelse(regData$Position %in% DEF,1,0))
+regData <- regData %>% mutate(MF_Bool = ifelse(regData$Position %in% MF,1,0))
+regData <- regData %>% mutate(ATT_Bool = ifelse(regData$Position %in% ATT,1,0))
+regData <- regData %>% mutate(Value = regressionData$ValueUnified)
+regData <- regData %>% mutate(Contract_Expiry = Contract_Expiry - min(Contract_Expiry))
+drop <- c("Position")
+regData <- regData[,!(names(regData) %in% drop)]
+
+##### Linear Regression
+
+set.seed(2017)
+
 library(MASS)
-library(neuralnet)
 
 index <- sample(1:nrow(regressionData),round(0.75*nrow(regressionData)))
 
-maxs <- apply(regressionData, 2, max) 
-mins <- apply(regressionData, 2, min)
+train <- regData[index,]
+test <- regData[-index,]
 
-scaled <- as.data.frame(scale(regressionData, center = mins, scale = maxs - mins))
+lm.fit <- glm(Value~., data = train)
+summary(lm.fit)
+predictionofLinearModel <- predict(lm.fit,test)
 
-valueDataset <- scaled %>% select(-WageUnified)
-wageDataset  <- scaled %>% select(-ValueUnified)
+MSEofLinearModel <- sum((predictionofLinearModel - test$Value)^2)/nrow(test)
 
-trainValue <- valueDataset[index,]
-testValue <- valueDataset[-index,]
+##### Neural network
+
+library(neuralnet)
+
+maxs <- apply(regData, 2, max)
+mins <- apply(regData, 2, min)
+
+scaled <- as.data.frame(scale(regData, center = mins, scale = maxs - mins))
+
+trainValue <- scaled[index,]
+testValue <- scaled[-index,]
 
 nValue <- names(trainValue)
-fValue <- as.formula(paste("ValueUnified ~", paste(nValue[!nValue %in% "ValueUnified"], collapse = " + ")))
-nnValue <- neuralnet(fValue,data=trainValue,hidden=c(6,3),linear.output=F)
+fValue <- as.formula(paste("Value ~", paste(nValue[!nValue %in% "Value"], collapse = " + ")))
+nnValue <- neuralnet(fValue, trainValue, hidden = c(12,12), threshold = 0.01,
+                     stepmax = 1e+05, rep = 1, startweights = NULL,
+                     learningrate.limit = NULL,
+                     learningrate.factor = list(minus = 0.5, plus = 1.2),
+                     learningrate=NULL, lifesign = "full",
+                     lifesign.step = 1000, algorithm = "rprop+",
+                     err.fct = "sse", act.fct = "logistic",
+                     linear.output = TRUE, exclude = NULL,
+                     constant.weights = NULL, likelihood = FALSE)
 
-trainWage <- wageDataset[index,]
-testWage <- wageDataset[-index,]
+prNNValue <- compute(nnValue,testValue[,1:17])
 
-nWage <- names(trainWage)
-fWage <- as.formula(paste("WageUnified ~", paste(nWage[!nWage %in% "WageUnified"], collapse = " + ")))
-nnWage <- neuralnet(fWage,data=trainWage,hidden=c(6,3),linear.output=F)
+predictionNNValues <- prNNValue$net.result*(max(regData$Value)-min(regData$Value))+min(regData$Value)
+testValues <- (testValue$Value)*(max(regData$Value)-min(regData$Value))+min(regData$Value)
+
+MSEofNN <- sum((testValues - predictionNNValues)^2)/nrow(testValue)
+
+par(mfrow=c(1,2))
+
+plot(test$Value,predictionNNValues,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend='NN',pch=18,col='red', bty='n')
+
+plot(test$Value,predictionofLinearModel,col='blue',main='Real vs predicted LM',pch=18, cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend='LM',pch=18,col='blue', bty='n', cex=.95)
+
+########################################
+########################################
+########################################
 
 getValuePrediction <- function(contract_expiry, rating, height, weight, age) {
   predictionData <- data.frame(contract_expiry, rating, height, weight, age)
@@ -59,7 +114,3 @@ getWagePrediction <- function(contract_expiry, rating, height, weight, age) {
   pr.nn_ <- pr.nn$net.result*(max(regressionData$WageUnified)-min(regressionData$WageUnified))+min(regressionData$WageUnified)
   return(pr.nn_)
 }
-
-
-testingtheprediction <- getValuePrediction(2018, 50, 180, 80, 20)
-testingtheprediction2 <- getWagePrediction(2018, 50, 180, 80, 20)
